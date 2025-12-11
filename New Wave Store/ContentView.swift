eb120a8d0ccf96c8b4032518b522c4b98e90e10b
Dany Malaeb <danymal12321@gmail.com>
2025-12-11 12:03:48 -0500

    Initial Commit

---
+++ New Wave Store/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New Wave Store
+//
+//  Created by Dany Malaeb (student LM) on 12/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
